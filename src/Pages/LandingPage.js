1221e1b07d9a49898b445a5dbae66a07c63ebd17
HumeraManjra <humera.1@hotmail.com>
2022-10-09 22:44:35 -0400

    landing page created

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Assessments from "../components/Assessments/Assessments";
+
+function LandingPage() {
+  return (
+    <>
+      <Header />
+      <Assessments />
+      <Footer />
+    </>
+  );
+}
+
+export default LandingPage;
